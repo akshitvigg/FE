d70553baab6345f7be164dee3c52f16dfb234966
Akshit Vig <akshitvig48@gmail.com>
2025-07-16 23:52:14 +0530

    fda

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className=" h-screen text-white flex">
-      <div className=" w-[380px] bg-[#181818]">dss</div>
+      <div className=" flex justify-between w-[380px] bg-[#181818]">
+        <p>dss</p> <button>slide</button>
+      </div>
       <div className=" w-full text-xl bg-[#212121]">
         <div></div>
       </div>
