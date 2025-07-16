e33a138ea796f5c8ddf6f4f24048fdc84f7b050c
Akshit Vig <akshitvig48@gmail.com>
2025-07-16 23:53:23 +0530

    dsds

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className=" h-screen text-white flex">
-      <div className=" flex justify-between w-[380px] bg-[#181818]">
-        <p>dss</p> <button>slide</button>
+      <div className="  w-[380px] bg-[#181818]">
+        <button>slide</button>
       </div>
       <div className=" w-full text-xl bg-[#212121]">
         <div></div>
